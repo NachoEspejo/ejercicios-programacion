1a3a23a663d0e95cc3d19ac1d0c0281c42be41fc
Nacho Espejo García <neg1999@hotmail.com>
2017-10-11 10:47:16 +0200

    Últimos retoques.

--- Ejercicios/Tema4/E01/Ejercicio01.java
+++ Ejercicios/Tema4/E01/Ejercicio01.java
@@ -3,7 +3,7 @@
  *Escribe un programa que pida por teclado un día de la semana y que diga qué
  *asignatura toca a primera hora ese día.
  */
- 
+
 public class Ejercicio01 {
   public static void main(String[] args) {
 
