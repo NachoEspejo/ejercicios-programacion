ca32f81257d709fe6abf2d692cf04fc3692a9da4
Nacho Espejo García <neg1999@hotmail.com>
2017-10-18 10:29:51 +0200

    Añadidos ejercicios T5.

--- Ejercicios/Tema5/E11/Ejercicio11.java
+++ Ejercicios/Tema5/E11/Ejercicio11.java
@@ -12,7 +12,11 @@ public class Ejercicio11 {
     int numero = Integer.parseInt(System.console().readLine());
     System.out.println("Procesando...");
     
+    System.out.println("╔════════════════════════════════╗");
+    System.out.println("   Base     Base²      Base³   ");
+    
     for (int n = numero; n < numero + 5; n++) {
+      
       System.out.println("╔════════════════════════════════╗");
       System.out.printf("%6d %8d %10d\n", n, n * n, n * n * n);
       System.out.println("╚════════════════════════════════╝");
