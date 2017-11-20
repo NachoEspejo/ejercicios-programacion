e9531dd0e31788a2fb598c05a5a22bb34d77971e
Nacho Espejo García <neg1999@hotmail.com>
2017-11-20 10:43:46 +0100

    Retoques y ejercicios nuevos T6.

--- Ejercicios/Tema6/Ejercicios/Ejercicio20.java
+++ Ejercicios/Tema6/Ejercicios/Ejercicio20.java
@@ -15,14 +15,14 @@ public class Ejercicio20 {
         
     for (int i = 0; i < capacidad; i++) {
       if (i < (capacidad - litros)) {
-        System.out.println("#    #");
+        System.out.println("┃    ┃");
       } else {
-        System.out.println("#====#");
+        System.out.println("┃▒▒▒▒┃"); 
       }
     }
         
-    System.out.println("######"); // fondo de la cuba
+    System.out.println("┗━━━━┛"); // fondo de la cuba
     System.out.print("La cuba tiene una capacidad de " + capacidad);
-    System.out.print(" litros y contiene " + litros + " litros de coca líquida.");
+    System.out.print(" litros y contiene " + litros + " litros de coca líquida  .");
   }
 }
