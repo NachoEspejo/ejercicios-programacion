50e70b3c4ff4b5e896873501ba5642f2e0c372c4
Nacho Espejo García <neg1999@hotmail.com>
2017-12-11 09:09:47 +0100

    Retoques.

--- Ejercicios/Tema7/Ejercicios/Ejercicios_Bidimensional/Ejercicio03.java
+++ Ejercicios/Tema7/Ejercicios/Ejercicios_Bidimensional/Ejercicio03.java
@@ -1,12 +1,11 @@
 /**
- *Ejercicio 2
  *Escribe un programa que pida 20 números enteros. Estos números se deben
  *introducir en un array de 4 filas por 5 columnas. El programa mostrará las
  *sumas parciales de filas y columnas igual que si de una hoja de cálculo se
  *tratara. La suma total debe aparecer en la esquina inferior derecha.
  */
 
-public class Ejercicio02 {
+public class Ejercicio03 {
   public static void main(String[] args) {
  
     int[][] num = new int[4][5]; // array de 4 filas por 5 columnas
