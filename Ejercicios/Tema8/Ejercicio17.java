d6e8b0346de31148a9135bcb36fca670f121e241
Nacho Espejo García <neg1999@hotmail.com>
2018-01-15 10:55:37 +0100

    Añadiendo ejercicios T8.

---
+++ Ejercicios/Tema8/Ejercicio17.java
@@ -0,0 +1,26 @@
+/**
+ * 8. Funciones
+ * 
+ * Ejercicio 17
+ *
+ */
+import Matemáticas.Varias;
+
+public class Ejercicio17 {
+  
+  public static void main(String[] args) {
+
+    long decimal = 0;
+    
+    System.out.print("Introduce un número binario: ");
+    long binario = Long.parseLong(System.console().readLine());
+    
+    int bits = Varias.digitos(binario);
+    
+    for(int i = 0; i < bits; i++) {
+      decimal += Varias.digitoN(binario, bits - i - 1) * Varias.potencia(2, i);
+    }
+    
+    System.out.println("El " + binario + " en binario, es " + decimal + " en decimal.");
+  }
+}
