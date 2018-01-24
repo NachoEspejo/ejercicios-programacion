6715bf7f3637a6b514389148f8d6d44be565e7b8
Nacho Espejo García <neg1999@hotmail.com>
2018-01-24 09:08:47 +0100

    Retoques.

--- Ejercicios/Tema8/Ejercicio20a28.java
+++ Ejercicios/Tema8/Ejercicio20a28.java
@@ -12,19 +12,19 @@ public class Ejercicio20a28 {
 
   public static void main(String[] args) {
     
-    // Ejercicio 21
+    // Ejercicio 20
     System.out.print("Array original: ");
     int[] a = funciones.Arrays.generaArrayInt(20, 0, 99);
     funciones.Arrays.muestraArrayInt(a);   
     System.out.println();
      
-    // Ejercicio 22,23,24
+    // Ejercicio 21,22,23
     System.out.println("Mínimo: " + funciones.Arrays.minimoArrayInt(a));
     System.out.println("Máximo: " + funciones.Arrays.maximoArrayInt(a));
     System.out.println("Media: " + funciones.Arrays.mediaArrayInt(a));
     System.out.println();
     
-    // Ejercicio 25
+    // Ejercicio 24
     if (funciones.Arrays.estaEnArrayInt(a, 69)) {
       System.out.println("El 69 está en el array.");
     } else {
@@ -33,24 +33,24 @@ public class Ejercicio20a28 {
     System.out.println();
     
     
-    // Ejercicio 26
+    // Ejercicio 25
     System.out.println("El 69 está en la posición " + funciones.Arrays.posicionEnArrayInt(a, 69));
     System.out.println();
     
     
-    // Ejercicio 27
+    // Ejercicio 26
     System.out.print("Array al revés: ");
     funciones.Arrays.muestraArrayInt(funciones.Arrays.volteaArrayInt(a));
     System.out.println();
     
     
-    // Ejercicio 28
+    // Ejercicio 27
     System.out.print("Array rotado 3 pasos a la derecha: ");
     funciones.Arrays.muestraArrayInt(funciones.Arrays.rotaDerechaArrayInt(a, 3));
     System.out.println();
     
     
-    // Ejercicio 29
+    // Ejercicio 28
     System.out.print("Array rotado 4 pasos a la izquierda: ");
     funciones.Arrays.muestraArrayInt(funciones.Arrays.rotaIzquierdaArrayInt(a, 4));
   }
